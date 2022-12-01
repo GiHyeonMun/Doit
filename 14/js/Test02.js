3afcbf37e31556391af4c185eedb5bf0ff6daa10
문기현 <john7266@naver.com>
2022-12-01 12:06:35 +0900

    20221201-jsResult

---
+++ 14/js/Test02.js
@@ -0,0 +1,23 @@
+    const memNum = parseInt(prompt("입장객 명수"));
+    const colNum = parseInt(prompt("1열당 수용가능 명수"));
+
+    const seatNum = memNum % colNum === 0 ? memNum / colNum : memNum / colNum + 1;
+
+    if(memNum && colNum){
+        for(let i = 0; i < seatNum; i++){
+            document.write('<tr>');
+            MakeRowSeat(i);
+            document.write('</tr>');
+        }
+    } else {
+        document.write("잘못된 값을 입력")
+    }
+
+    function MakeRowSeat(nowRow) {
+        for(let x = 0; x < colNum; x++){
+            const SeatNo = nowRow * colNum + x;
+            if(seatNum > memNum) break;
+            document.write('<td>좌석 ' + SeatNo + '</td>');
+        }
+        return true;
+    }
